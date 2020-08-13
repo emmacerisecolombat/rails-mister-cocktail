cabb4735ce222b8d1139d11a0a91bf7e2b3009c2
Emma Colombat <emma.colombat69@gmail.com>
2020-08-13 15:10:53 +0200

    created seeds and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+# # 1. COCKTAIL ROUTER
+#   # A user can see the list of cocktails
+#     GET "/cocktails", to: "coktails#index"
+#   # A user can see the details of a given cocktail, with the dose needed for each ingredient
+#     GET "/cocktails/:id", to: "cocktails#show"
+#   # A user can create a new cocktail
+#     GET "/cocktails/new", to: "cocktails#new"
+#     POST "/cocktails", to: "cocktails#create"
+  resources :cocktails, only: [:index, :show, :new, :create]
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "🍹🍸Creating 100 fake ingredients..."
+  uri = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
+  ingredient_array_of_hash = JSON.parse(uri)["drinks"]
+  ingredient_array_of_hash.each do |ingredient_hash|
+    puts ingredient_name = ingredient_hash.values
+    Ingredient.create(name:ingredient_name)
+  end
+
+  # path = File.join(File.dirname("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"), "./seeds/cocktails.json")
+  # records = JSON.parse(File.read(path))
+  # records.each do |record|
+  #   Cocktail.create!(record)
+  # end
+puts "Cocktails are seeded, CHEEEERSS! #{Ingredient.count} ingredients created"
+
